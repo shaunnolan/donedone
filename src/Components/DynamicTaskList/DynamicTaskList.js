ba67a2bc0e6b14080467d218a4d24e5f09babfaf
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-02 20:42:53 +0000

    new parent container for task components

---
+++ src/Components/DynamicTaskList/DynamicTaskList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddTask from '../AddTask/AddTask'
+import TaskList from '../TaskList/TaskList'
+
+function DynamicTaskList() {
+    return (
+        <>
+        <AddTask />
+        <TaskList />
+        </>
+    )
+}
+
+export default DynamicTaskList
