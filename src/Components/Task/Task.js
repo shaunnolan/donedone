f6b797af353128cd4cd51fe62518f6df7d93c3eb
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-03 21:16:47 +0000

    add bootstrap card layout and classes to tasks

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function Task(props) {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
+    <div className="col-sm-6 col-lg-4 mb-4">
+      <div className="card">
+        <div className="card-body">
+          <h5>{props.title}</h5>
+          <p className="card-text">{props.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
