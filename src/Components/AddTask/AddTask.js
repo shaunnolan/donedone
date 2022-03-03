9b3ac82b63358e37b71cf6e1ebd25e267a611aeb
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-03 21:37:11 +0000

    markup cleanup and new style to provide spacing

--- src/Components/AddTask/AddTask.js
+++ src/Components/AddTask/AddTask.js
@@ -1,3 +1,4 @@
+import "./AddTask.css";
 import React, { useState } from "react";
 
 export default function AddTask(props) {
@@ -13,23 +14,21 @@ export default function AddTask(props) {
   };
 
   return (
-    <div>
-      <form className="row g-2">
-        <div className="col">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Add another task"
-            value={newTask}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div className="col-auto">
-          <button className="btn btn-primary" onClick={addTask}>
-            Add Task
-          </button>
-        </div>
-      </form>
-    </div>
+    <form className="row g-2 addTaskForm">
+      <div className="col">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Add another task"
+          value={newTask}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div className="col-auto">
+        <button className="btn btn-primary" onClick={addTask}>
+          Add Task
+        </button>
+      </div>
+    </form>
   );
 }
