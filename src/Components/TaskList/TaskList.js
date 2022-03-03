183f3283389b2c82ffb50090ee5433894da049d7
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-03 20:47:28 +0000

    clean up mapping of tasks

--- src/Components/TaskList/TaskList.js
+++ src/Components/TaskList/TaskList.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Task from "../Task/Task";
 
 export default function TaskList(props) {
-  return (
-    props.tasks &&
-    props.tasks.map((task) => {
-      return <Task title={task.title} description={task.description} />;
-    })
-  );
+  return props.tasks.map((task) => (
+    <Task title={task.title} description={task.description} />
+  ));
 }
