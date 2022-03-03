06a21feea1431ec635479366c80be646cdd02ea3
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-03 21:17:10 +0000

    add bootstrap classes and layout to tasklist

--- src/Components/TaskList/TaskList.js
+++ src/Components/TaskList/TaskList.js
@@ -2,7 +2,11 @@ import React from "react";
 import Task from "../Task/Task";
 
 export default function TaskList(props) {
-  return props.tasks.map((task) => (
-    <Task title={task.title} description={task.description} />
-  ));
+  return (
+    <div className="row" data-masonry='{"percentPosition": true}'>
+      {props.tasks.map((task) => (
+        <Task title={task.title} description={task.description} />
+      ))}
+    </div>
+  );
 }
