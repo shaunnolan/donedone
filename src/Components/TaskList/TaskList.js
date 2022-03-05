f811782ef7988fce35e32037f7ed1f9181511f9d
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-05 20:15:25 +0000

    fix ui bug when adding new tasks

--- src/Components/TaskList/TaskList.js
+++ src/Components/TaskList/TaskList.js
@@ -1,9 +1,15 @@
-import React from "react";
+import Masonry from "masonry-layout";
+import React, { useEffect } from "react";
 import Task from "../Task/Task";
 
 export default function TaskList(props) {
+  useEffect(() => {
+    const masonry = new Masonry(".masonry-grid");
+    masonry.layout();
+  });
+
   return (
-    <div className="row" data-masonry='{"percentPosition": true}'>
+    <div className="row masonry-grid">
       {props.tasks.map((task) => (
         <Task title={task.title} description={task.description} />
       ))}
