39db6c022203f71bf83a5514d36eb1320dcf14ca
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-06 20:58:13 +0000

    simplify events for tasks
    don't need state to get the keydown event to work properly.
    the keydown will only fire if the mouse is over the element.

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,15 +1,29 @@
 import React from "react";
-import "masonry-layout/masonry";
 
 export default function Task(props) {
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 88) {
+      console.log("deleted");
+    }
+  };
+
   return (
     <div className="col-sm-6 col-lg-4 mb-4">
-      <div className="card">
-        <div className="card-body">
-          <h5>{props.title}</h5>
-          <p className="card-text">{props.description}</p>
+      <a className="task" href="#" onKeyDown={handleKeyDown} tabIndex="-1">
+        <div className="card">
+          <div className="card-header">
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="card-body">
+            <h5>{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
