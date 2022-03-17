31f6316cca4bf7aa3dbce984b829584fde8768d2
Shaun Nolan <shaun.nolan@gmail.com>
2022-03-17 21:28:37 +0000

    remove card header

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -11,13 +11,6 @@ export default function Task(props) {
     <div className="col-sm-6 col-lg-4 mb-4">
       <a className="task" href="#" onKeyDown={handleKeyDown} tabIndex="-1">
         <div className="card">
-          <div className="card-header">
-            <button
-              type="button"
-              className="btn-close"
-              aria-label="Close"
-            ></button>
-          </div>
           <div className="card-body">
             <h5>{props.title}</h5>
             <p className="card-text">{props.description}</p>
